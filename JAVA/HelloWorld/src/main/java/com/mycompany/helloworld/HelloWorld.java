e3280bba18be356ca426e5887f420e8362c1d633
Cento <tanhiep26@gmail.com>
2022-06-12 14:45:07 +0700

    Upload

---
+++ JAVA/HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.mycompany.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int myNum = 15;
+        final int aFinnalNum = 15; //finnal number can not be chance afterward
+        System.out.println("Hello World!"); //Out put text and add a new line 
+        System.out.print("Is this on the same line");   //Print text but doesn't add new line to console
+        
+        System.out.println(myNum);
+        
+        System.out.println(aFinnalNum);
+        
+        
+    }
+}
